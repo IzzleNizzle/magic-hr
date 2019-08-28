74cde226e4e72a5f0545721d5ef49f8e77d7b0ba
Iz P <pehisaac@gmail.com>
2019-08-27 18:39:39 -0600

    Trying to bring in Feedbackizzle

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
+import FeedbackIzzle from 'feedbackizzle'
 
 function App() {
   return (
     <Router>
       <div>
         <Nav />
+        <FeedbackIzzle />
         <Switch>
           <Route exact path="/" component={Home} />
           <Route component={NoMatch} />
